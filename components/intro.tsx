9a32d1ac3e07838695ffc33a4fddc3ac67a2b877
Viktor <sinvvvvv1@gmail.com>
2024-12-27 13:51:10 +0200

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,8 +65,8 @@ export default function Intro() {
         <span className="font-bold">Hello, I'm Viktor.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">2 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        building <span className="italic">websites & applications</span>. My focus is on{" "}
+        <span className="underline">React (Next.js)</span> and <span className="underline">Node.js (NestJS)</span>. 
       </motion.h1>
 
       <motion.div
